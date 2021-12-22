98ef01d0f7d44be0011aefc15fdc0129d64428c9
Sunder Rawat <sunderrawat777@gmail.com>
2021-12-22 18:42:47 +0530

    redux added for cart

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -1,20 +1,17 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { cartActions } from "../store/cart";
 import "./ProductItem.css";
 function ProductItem(props) {
-function cartAddHandler(e){
-    console.log('Item Added to Cart successfully');
-}
-// category: "men's clothing"
-// description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
-// id: 1
-// image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-// price: 109.95
-// rating: {rate: 3.9, count: 120}
-// title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+  const dispatch = useDispatch();
+  function cartAddHandler(item) {
+    dispatch(cartActions.cartDataHandler(item));
+    console.log("Item Added to Cart successfully");
+  }
   return (
     <div className="container">
       <div className="imgbox">
-        <img src={props.data.image} alt={props.data.title} className="img"/>
+        <img src={props.data.image} alt={props.data.title} className="img" />
       </div>
       <div className="detailsbox">
         <h2 className="title">{props.data.title}</h2>
@@ -22,7 +19,14 @@ function cartAddHandler(e){
         <p className="description">{props.data.description}</p>
       </div>
       <div className="buttonbox">
-        <button className="btn" onClick={cartAddHandler}>Add</button>
+        <button
+          className="btn"
+          onClick={() => {
+            cartAddHandler(props.data);
+          }}
+        >
+          Add
+        </button>
       </div>
     </div>
   );
---
+++ src/store/cart.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  totalCartItem: 0,
+  cartAmount: 0,
+  cartData:[]
+};
+
+const cartSlice = createSlice({
+  name: "cartData",
+  initialState,
+  reducers: {
+    cartAmountHandler(state, action) {
+      state.cartAmount = action.payload;
+    },
+    totalCarItemHandler(state, action) {
+      state.totalCartItem = action.payload;
+    },
+    cartDataHandler(state, action){
+        state.totalCartItem = action.payload;
+    }
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
